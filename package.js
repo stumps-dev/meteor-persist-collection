ab4abff9363ce000181ff4c4cbc07a1e17e5e0a4
Stumps <35468906+stumpss@users.noreply.github.com>
2018-01-19 16:59:16 +0100

    Changed package versions

--- package.js
+++ package.js
@@ -1,6 +1,6 @@
 Package.describe({
   name: 'stumps:persist-collection',
-  version: '1.0.2',
+  version: '1.1.0',
   summary: 'Persist a collection for offline use.',
   git: 'https://github.com/stumpss/meteor-persist-collection',
   documentation: 'README.md'
@@ -9,10 +9,11 @@ Package.describe({
 Package.onUse(function(api) {
 
   api.use([
-    'underscore@1.0.10',
-    'ecmascript@0.9.0',
-    'mongo@1.1.24',
-    'reactive-var@1.0.11'
+    'underscore@1.0.0',
+    'ecmascript@0.1.3',
+    'mongo@1.0.4',
+    'reactive-var@1.0.1',
+    'random@1.0.0'
   ])
 
   api.addFiles('persist-collection.js', 'client')
